818f97c1e60af0449335eea392f92f2c9547eb0f
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-05-23 21:42:06 +0500

    commit of day

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./App.css"
 
-const App = () => {
+const App = () => 
+  {
+
+    const [screen, setScreen] = useState(1);
+
   return (
-    <div>App</div>
+    <>
+    {
+      screen === 1 ?
+      <>
+        <div className="container flex flex-col items-center justify-center h-screen">
+          <h1 className='text-[30px] font-[700]'>AI-Powered <span className='text bg-green-700'>Blog Generator</span></h1>
+          <textarea className='w-[50vw] min-h-[50vh] mt-5 p-[30px] bg-transparent border-[2px] border-[#929292ad] focus:border-green-900 outline-none rounded-xl' placeholder='Enter your Prompt...' ></textarea>
+
+
+        </div>
+      </> 
+      : 
+      <>Screen 2</>
+    }
+    </>
   )
 }
 
